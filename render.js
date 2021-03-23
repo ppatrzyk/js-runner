1ac13f2a29667df9addab1381a9b0e3ce335203b
Piotr Patrzyk <piotr@patrzyk.me>
2021-03-23 18:48:26 +0100

    init jsdom

--- render.js
+++ render.js
@@ -4,8 +4,10 @@ const { JSDOM } = jsdom;
 function render(url) {
     (async () => {
         console.log(url);
-        // html = dom.serialize();
+        dom = await JSDOM.fromURL(url, {})
+        html = dom.serialize();
+        console.log(html)
     })();
 }
 
-render("test print");
+render("https://ppatrzyk.github.io/foreign-tourists");
