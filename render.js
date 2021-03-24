2806f06e90181521784d18a77452592b7ba5c43a
Piotr Patrzyk <piotr@patrzyk.me>
2021-03-24 18:05:43 +0100

    script get attempt 1

--- render.js
+++ render.js
@@ -1,10 +1,15 @@
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
+const options = {
+    resources: "usable",
+    runScripts: "dangerously",
+};
+
 function render(url) {
     (async () => {
         console.log(url);
-        dom = await JSDOM.fromURL(url, {})
+        dom = await JSDOM.fromURL(url, options)
         html = dom.serialize();
         console.log(html)
     })();
