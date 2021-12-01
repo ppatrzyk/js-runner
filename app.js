55ebd3416254862c3a0b4d6631596476ecb8cb8b
Piotr Patrzyk <piotr@patrzyk.me>
2021-12-01 16:02:40 +0100

    read request body

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const Koa = require('koa');
-const app = new Koa();
+const koa = require('koa');
+const koa_body = require('koa-body');
+
+const app = new koa();
+app.use(koa_body());
 
 // response
 app.use(ctx => {
   if (ctx.path == '/render' & ctx.method == 'POST') {
     console.log(ctx.request);
+    console.log(ctx.request.body);
     ctx.body = 'Hello Koa'; 
   } else {
     ctx.throw(404)
