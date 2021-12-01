c01e318f347c8c08fb31b393fa15d04a277090ad
Piotr Patrzyk <piotr@patrzyk.me>
2021-12-01 16:16:34 +0100

    read args

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ app.use(koa_body());
 // response
 app.use(ctx => {
   if (ctx.path == '/render' & ctx.method == 'POST') {
-    console.log(ctx.request);
-    console.log(ctx.request.body);
+    html = ctx.request.body.html
+    scripts = ctx.request.body.scripts
+    console.log(html)
     ctx.body = 'Hello Koa'; 
   } else {
     ctx.throw(404)
