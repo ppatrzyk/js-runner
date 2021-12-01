81d8118ae4c4439c0325591159f2b94e6d397484
Piotr Patrzyk <piotr@patrzyk.me>
2021-12-01 15:36:53 +0100

    test script running

--- render.js
+++ render.js
@@ -8,8 +8,11 @@ const options = {
 
 function render(html, scripts) {
     dom = new JSDOM(html, options)
+    scripts.forEach(script => {
+        dom.window.eval(script);
+    });
     rendered_html = dom.serialize();
     console.log(rendered_html)
 }
 
-render(`<!DOCTYPE html><p>Hello world</p>`, []);
+render(`<!DOCTYPE html><p>Hello world</p>`, ['document.body.appendChild(document.createElement("hr"));']);
