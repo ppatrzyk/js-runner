f16f0eb4aa5cfd6a68f1eb323497ab27e6a637a1
Piotr Patrzyk <piotr@patrzyk.me>
2021-12-04 17:15:45 +0100

    no external scripts evaluation

--- render.js
+++ render.js
@@ -9,11 +9,8 @@ const options = {
 
 const fetch_js = fs.readFileSync(require.resolve('whatwg-fetch/fetch.js'), 'utf-8')
 
-function render(html, scripts) {
+function render(html, url) {
     dom = new JSDOM(html, options)
-    [fetch_js].concat(scripts).forEach(script => {
-        dom.window.eval(script);
-    });
     rendered_html = dom.serialize();
     return rendered_html
 }
