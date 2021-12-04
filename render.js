935cc1be8a45656e9b6c3b596559baff89639cd0
Piotr Patrzyk <piotr@patrzyk.me>
2021-12-04 17:50:17 +0100

    fetch script insert attempt

--- render.js
+++ render.js
@@ -8,10 +8,12 @@ const options = {
     runScripts: "dangerously",
 };
 
-const fetch_js = fs.readFileSync(require.resolve('whatwg-fetch/fetch.js'), 'utf-8')
+const fetch_js = `<script type="module">${fs.readFileSync(require.resolve('whatwg-fetch/fetch.js'), 'utf-8')}</script>`
 
 function render(html, url) {
-    dom = new JSDOM(html, {...options, ...{"url": url}})
+    html_fix = html.replace('<head>', `<head>\n${fetch_js}`)
+    // console.log(html_fix)
+    dom = new JSDOM(html_fix, {...options, ...{"url": url}})
     rendered_html = dom.serialize();
     return rendered_html
 }
