2728d76539addfbe49df3f181894f771c2f18f6e
Piotr Patrzyk <piotr@patrzyk.me>
2021-12-09 17:46:18 +0100

    virt console

--- render.js
+++ render.js
@@ -11,9 +11,12 @@ const options = {
 const fetch_js = `<script type="module">${fs.readFileSync('./fetch/fetch.umd.js', 'utf-8')}</script>`
 
 function render(html, url) {
+    // TODO is it passed correctly
+    const jsdom_console = new jsdom.VirtualConsole();
+    jsdom_console.sendTo(console);
     html_fix = html.replace('<head>', `<head>\n${fetch_js}`)
     // console.log(html_fix)
-    dom = new JSDOM(html_fix, {...options, ...{"url": url}})
+    dom = new JSDOM(html_fix, {...options, ...{"url": url, jsdom_console}})
     rendered_html = dom.serialize();
     return rendered_html
 }
