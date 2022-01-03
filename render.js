c64c90d1620eccccf040d05a3edf1512c94d4e40
Piotr Patrzyk <piotr@patrzyk.me>
2022-01-03 14:12:02 +0100

    rm whatwg fetch; add unfetch

--- render.js
+++ render.js
@@ -8,13 +8,14 @@ const options = {
     runScripts: "dangerously",
 };
 
-const fetch_js = `<script type="module">${fs.readFileSync('./fetch/fetch.umd.js', 'utf-8')}</script>`
+// https://github.com/developit/unfetch
+const unfetch = `<script>${fs.readFileSync('./unfetch-4.2.0.js', 'utf-8')}</script>`
 
 function render(html, url) {
     // TODO is it passed correctly
     const jsdom_console = new jsdom.VirtualConsole();
     jsdom_console.sendTo(console);
-    html_fix = html.replace('<head>', `<head>\n${fetch_js}`)
+    html_fix = html.replace('<head>', `<head>\n${unfetch}`)
     // console.log(html_fix)
     dom = new JSDOM(html_fix, {...options, ...{"url": url, virtualConsole: jsdom_console}})
     rendered_html = dom.serialize();
