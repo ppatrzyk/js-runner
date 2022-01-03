f781a457b0b5cbb2e9c724a0295fd977db4c5da4
Piotr Patrzyk <piotr@patrzyk.me>
2022-01-03 14:22:23 +0100

    rm console

--- render.js
+++ render.js
@@ -12,12 +12,9 @@ const options = {
 const unfetch = `<script>${fs.readFileSync('./unfetch-4.2.0.js', 'utf-8')}</script>`
 
 function render(html, url) {
-    // TODO is it passed correctly
-    const jsdom_console = new jsdom.VirtualConsole();
-    jsdom_console.sendTo(console);
     html_fix = html.replace('<head>', `<head>\n${unfetch}`)
     // console.log(html_fix)
-    dom = new JSDOM(html_fix, {...options, ...{"url": url, virtualConsole: jsdom_console}})
+    dom = new JSDOM(html_fix, {...options, ...{"url": url}})
     rendered_html = dom.serialize();
     return rendered_html
 }
