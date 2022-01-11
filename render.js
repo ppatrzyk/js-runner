78a51df0afb3644c13681728c6f2f2df811af846
Piotr Patrzyk <piotr@patrzyk.me>
2022-01-11 19:38:12 +0100

    Revert "rm console"
    This reverts commit f781a457b0b5cbb2e9c724a0295fd977db4c5da4.

--- render.js
+++ render.js
@@ -12,9 +12,12 @@ const options = {
 const unfetch = `<script>${fs.readFileSync('./unfetch-4.2.0.js', 'utf-8')}</script>`
 
 function render(html, url) {
+    // TODO is it passed correctly
+    const jsdom_console = new jsdom.VirtualConsole();
+    jsdom_console.sendTo(console);
     html_fix = html.replace('<head>', `<head>\n${unfetch}`)
     // console.log(html_fix)
-    dom = new JSDOM(html_fix, {...options, ...{"url": url}})
+    dom = new JSDOM(html_fix, {...options, ...{"url": url, virtualConsole: jsdom_console}})
     rendered_html = dom.serialize();
     return rendered_html
 }
