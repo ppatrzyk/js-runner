039c86c542942651081d907d6640dded620454b1
Piotr Patrzyk <piotr@patrzyk.me>
2022-01-11 20:06:41 +0100

    timer v1 init

--- render.js
+++ render.js
@@ -11,6 +11,10 @@ const options = {
 // https://github.com/developit/unfetch
 const unfetch = `<script>${fs.readFileSync('./unfetch-4.2.0.js', 'utf-8')}</script>`
 
+// https://stackoverflow.com/a/46937705
+const timer = ms => new Promise( res => setTimeout(res, ms));
+const render_wait = 5000
+
 function render(html, url) {
     // TODO is it passed correctly
     const jsdom_console = new jsdom.VirtualConsole();
@@ -18,6 +22,7 @@ function render(html, url) {
     html_fix = html.replace('<head>', `<head>\n${unfetch}`)
     // console.log(html_fix)
     dom = new JSDOM(html_fix, {...options, ...{"url": url, virtualConsole: jsdom_console}})
+    timer(render_wait).then(_=>console.log("rendering ok"));
     rendered_html = dom.serialize();
     return rendered_html
 }
