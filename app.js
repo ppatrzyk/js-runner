9e972507443b393a7f439f4c68a04b001630eb21
Piotr Patrzyk <piotr@patrzyk.me>
2022-01-12 16:50:28 +0100

    try except stuff

--- app.js
+++ app.js
@@ -8,17 +8,22 @@ const app = new koa();
 // response
 app.use(async ctx => {
   if (ctx.path == '/render' & ctx.method == 'POST') {
-    var start = Date.now();
-    var opts = {limit: '100Mb', strict: false};
-    var req_body = await parse.json(ctx.request, opts);
-    // todo maybe cookies in the future
-    // https://github.com/jsdom/jsdom#cookie-jars
-    ["html", "url", "user_agent", "render_wait"].forEach(key => {
-      assert(req_body.hasOwnProperty(key));
-    });
-    req_body.start_time = start;
-    body = await render(req_body)
-    ctx.body = body; 
+    try {
+      var start = Date.now();
+      var opts = {limit: '100Mb', strict: false};
+      var req_body = await parse.json(ctx.request, opts);
+      // todo maybe cookies in the future
+      // https://github.com/jsdom/jsdom#cookie-jars
+      ["html", "url", "user_agent", "render_wait"].forEach(key => {
+        assert(req_body.hasOwnProperty(key));
+      });
+      req_body.start_time = start;
+      body = await render(req_body)
+      ctx.body = body;
+    } catch (error) {
+      console.log(error)
+      ctx.throw(500)
+    }
   } else {
     ctx.throw(404)
   }
